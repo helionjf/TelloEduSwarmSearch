651b4c377e8b9f3601d104b92891591e20dfd268
helionjf <54320379+helionjf@users.noreply.github.com>
2020-01-17 18:58:09 +0100

    préparation demo amphi

--- demo_status.py
+++ demo_status.py
@@ -1,7 +1,11 @@
 from fly_tello import FlyTello
 my_tellos = list()
 
-
+'''
+// table à midi
+// 1=> au sol à 3 h
+// 2=> sur la table à 3 h
+'''
 #
 # SIMPLE EXAMPLE - MOST BASIC FLIGHT TO SHOW STATUS MESSAGES
 #
@@ -14,10 +18,13 @@ my_tellos = list()
 #
 
 # Define the Tello's we're using, in the order we want them numbered
-my_tellos.append('0TQDFC6EDBBX03')  # 1-Yellow
-my_tellos.append('0TQDFC6EDB4398')  # 2-Blue
-# my_tellos.append('0TQDFC6EDBH8M8')  # 3-Green
-# my_tellos.append('0TQDFC7EDB4874')  # 4-Red
+my_tellos.append('0TQDG2KEDB4FH3')  # numéro 1 == DC5CE0
+my_tellos.append('0TQDG2KEDBWK3X')  # numéro 2 == DC5F6C
+# my_tellos.append('0TQDFCHEDB3F86')  # numéro 3 == D3FCE4
+# my_tellos.append('0TQDG2KEDB04T1')  # numéro 4 == DC5CF3
+# my_tellos.append('0TQDFCHEDBY3H0')  # numéro 5 == D3F926
+# my_tellos.append('0TQDG2KEDBPE19')  # numéro 6 == DC5F05
+
 
 # Control the flight
 with FlyTello(my_tellos, get_status=True) as fly:

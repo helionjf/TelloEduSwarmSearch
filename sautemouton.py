60e176aa1cceae5b82d4126447c3a71e74624f18
helionjf <helionjf@gmail.com>
2020-01-19 17:53:52 +0100

    restructuration du programme

--- sautemouton.py
+++ sautemouton.py
@@ -3,14 +3,14 @@ my_tellos = list()
 
 '''
 // scenario specifique amphi MPG
-// position initiale : 1 2 espacés de 50 cm
+// position initiale : 1 2 espacés de 100 cm
 // 3 à 4 m d'espace
 // TO DO : à terminer 
 '''
 
 #
 # MAIN FLIGHT CONTROL LOGIC
-#
+
 
 # Define the Tello's we're using, in the order we want them numbered
 my_tellos.append('0TQDG2KEDB4FH3')  # numéro 1 == DC5CE0
@@ -23,34 +23,27 @@ my_tellos.append('0TQDG2KEDBWK3X')  # numéro 2 == DC5F6C
 
 # Control the flight
 with FlyTello(my_tellos, get_status=True) as fly:
-    # TO DO : battery_check 20
-    fly.get_battery()
     fly.print_status(sync=True)
-    fly.takeoff()
-    fly.print_status(sync=True)
-    # TO DO : ccw 90
-    fly.up(80,1)
-    fly.rotate_ccw(360,2)
-    fly.forward(100,1)
-    fly.down(80,1)
-    fly.wait_sync()
-    fly.up(80,2)
-    fly.rotate_ccw(360,1)
-    fly.forward(100,2)
-    fly.down(80,2)
-    fly.wait_sync()
-    #fly.flip(f)
-    fly.back(200)
-    fly.print_status(sync=True)
-    # TO DO : up 120
-    #fly.print_status(sync=True)
-    # TO DO : 1>forward 100
-    #fly.print_status(sync=True)
-    # TO DO : sync 2
-    #fly.print_status(sync=True)
-    # TO DO : 2>back 100
-    #fly.print_status(sync=True)
-    # TO DO : cw 90
-    #fly.print_status(sync=True)
+    fly.takeoff(sync=True)
+    i,j = 1,2
+    saute(i,j)
+    i,j = 2,1
+    saute(i,j)
+    # fly.flip(f)
+    fly.back(200,sync=True)
     fly.land()
     fly.print_status(sync=True)
+
+
+def saute(i,j):
+        # Le i se lève de 100
+    fly.up(100,i)
+    # Le j tourne sur lui même
+    fly.rotate_ccw(360,j)
+    # Le i avance de 150
+    fly.forward(150,i)
+    # Le j descend de 150
+    fly.down(150,i)
+    # synchronise
+    fly.wait_sync()
+    # Premier tour

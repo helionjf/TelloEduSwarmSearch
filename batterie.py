d4a4ad6bee42eb63008d781f7e558dcd3c12fe2e
helionjf <helionjf@gmail.com>
2020-01-20 23:55:41 +0100

    Create batterie.py
    Permet d'obtenir le niveau de batterie quel que soit le nombre de drones

---
+++ batterie.py
@@ -0,0 +1,31 @@
+from fly_tello import FlyTello
+my_tellos = list()
+
+
+# SIMPLE EXAMPLE - MOST BASIC FLIGHT TO SHOW STATUS MESSAGES
+#
+# SETUP: Any number of Tellos
+#
+
+
+#
+# MAIN FLIGHT CONTROL LOGIC
+#
+
+# Define the Tello's we're using, in the order we want them numbered
+my_tellos.append('0TQDG2KEDB4FH3')  # numéro 1 == DC5CE0
+my_tellos.append('0TQDG2KEDBWK3X')  # numéro 2 == DC5F6C
+# my_tellos.append('0TQDFCHEDB3F86')  # numéro 3 == D3FCE4
+# my_tellos.append('0TQDG2KEDB04T1')  # numéro 4 == DC5CF3
+# my_tellos.append('0TQDFCHEDBY3H0')  # numéro 5 == D3F926
+# my_tellos.append('0TQDG2KEDBPE19')  # numéro 6 == DC5F05
+
+
+
+# parcours de la liste des tellos
+with FlyTello(my_tellos, get_status=True) as fly:
+    i = 0
+    while i < len(my_tellos):
+        fly.get_sn(i)
+        fly.get_battery(i)
+        i +=1
